2f238b14b5022d15874159d08351e42e004426c4
zhan <zhansj39@gmail.com>
2020-01-13 19:16:28 +0800

    post func

--- member/views.py
+++ member/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from .models import Post
+from .form import PostForm
+
 
 # Create your views here.
 def hello_view(request):
-    return render(request, 'hello_django.html', {'data': "hello django"})
+    # Without use form
+    # obj = Post.objects.create(name=request.POST['name'])
+    # obj.save()
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    member_data = Post.objects.all()
+    return render(request, 'hello_django.html', {'member_data': member_data})
 
 
 def member(request):
