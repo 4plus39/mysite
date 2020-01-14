5f2a4895e05d70e775341aba9df9f62e4d3a1959
zhan <zhansj39@gmail.com>
2020-01-14 18:35:23 +0800

    0114

--- member/models.py
+++ member/models.py
@@ -8,6 +8,7 @@ class Post(models.Model):
     number = models.CharField(max_length=10)
     email = models.CharField(max_length=30)
     created_date = models.DateTimeField(default=timezone.now)
+    updated_date = models.DateTimeField('更新時間', auto_now=True)
 
     def __str__(self):
         return self.name
